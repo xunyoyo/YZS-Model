c20fc391d128827cba2a4f46bf40f804211fda50
xunyoyo <1279416582@qq.com>
2024-02-05 17:03:09 +0800

    测试

--- smiles2topology.py
+++ smiles2topology.py
@@ -2,4 +2,13 @@ import os.path as op
 import numpy
 import torch
 from rdkit import Chem
+from rdkit.Chem import MolFromSmiles
 from tqdm import tqdm
+
+
+class DatasetProcess:
+    def __init__(self):
+        # 这我不会写...
+        pass
+
+    pass
