e1d9e4770d824c4c315404c9b70f0fac82dba746
xunyoyo <1279416582@qq.com>
2024-02-05 21:57:17 +0800

    测试

--- smiles2topology.py
+++ smiles2topology.py
@@ -3,9 +3,15 @@ import numpy
 import pandas as pd
 import torch
 from torch_geometric.data import InMemoryDataset
+from torch_geometric import data as DATA
+import networkx as nx
 from rdkit import Chem
 from rdkit.Chem import MolFromSmiles
+from rdkit.Chem import ChemicalFeatures
+from rdkit import RDConfig
 from tqdm import tqdm
+fdef_name = op.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
+chem_feature_factory = ChemicalFeatures.BuildFeatureFactory(fdef_name)
 
 
 class MyOwnDataset(InMemoryDataset):
@@ -28,20 +34,66 @@ class MyOwnDataset(InMemoryDataset):
     def download(self):
         pass
 
-    def pro_process(self, data_path, data_dict):
+    def pre_process(self, data_path, data_dict):
         file = pd.read_csv(data_path)
 
         data_lists = []
         for index, row in file.iterrows():
             smiles = row['isomeric_smiles']
             logS = row['logS0']
-            pass
-
-
-
 
             # 重构一下process方法
     def process(self):
-        pass
+        file_train = pd.read_csv(self.raw_paths[0])
+        file = file_train
 
-    pass
+        smiles = file['isomeric_smiles'].unique()
+        graph_dict = dict()
+
+        for smile in tqdm(smiles, total=len(smiles)):
+            mol = Chem.MolFromSmiles(smile)
+            g = self.mol2graph(mol)
+            graph_dict[smile] = g
+
+        # train_list =
+
+        # if self.pre_filter is not None:
+        #     train_list = [train for train in train_list if self.pre_filter(train)]
+        #     test_list = [test for test in test_list if self.pre_filter(test)]
+        #
+        # if self.pre_transform is not None:
+        #     train_list = [self.pre_transform(train) for train in train_list]
+        #     test_list = [self.pre_transform(test) for test in test_list]
+
+    def mol2graph(self, mol):
+        if mol is None:
+            return None
+
+        features = chem_feature_factory.GetFeaturesForMol(mol)
+        graph = nx.DiGraph()
+
+        for i in range(mol.GetNumAtoms()):
+            atom_i = mol.GetAtomWithIdx(i)
+            graph.add_node(i,
+                       # a_type=atom_i.GetSymbol(),
+                       # a_num=atom_i.GetAtomicNum(),
+                       # acceptor=0,
+                       # donor=0,
+                       # aromatic=atom_i.GetIsAromatic(),
+                       # hybridization=atom_i.GetHybridization(),
+                       # num_h=atom_i.GetTotalNumHs(),
+                       #
+                       # # 5 more node features
+                       # ExplicitValence=atom_i.GetExplicitValence(),
+                       # FormalCharge=atom_i.GetFormalCharge(),
+                       # ImplicitValence=atom_i.GetImplicitValence(),
+                       # NumExplicitHs=atom_i.GetNumExplicitHs(),
+                       # NumRadicalElectrons=atom_i.GetNumRadicalElectrons(),
+                       )
+
+
+        for i in range(len(features)):
+            pass
+
+        for i in range(mol.GetNumAtoms()):
+            pass
