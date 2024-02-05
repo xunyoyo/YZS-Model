d16b39092b3a6c6e4523b14f552fd7d5612e8811
xunyoyo <1279416582@qq.com>
2024-02-05 17:27:50 +0800

    测试

--- smiles2topology.py
+++ smiles2topology.py
@@ -8,12 +8,13 @@ from tqdm import tqdm
 
 
 class GNNDataset(InMemoryDataset):
-    def __init__(self):
-        def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None):
-            super().__init__(root, transform, pre_transform, pre_filter)
-            if train:
-                self.data, self.slices = torch.load(self.processed_paths[0])
-            else:
-                self.data, self.slices = torch.load(self.processed_paths[1])
+    def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        if train:
+            self.data, self.slices = torch.load(self.processed_paths[0])
+        else:
+            self.data, self.slices = torch.load(self.processed_paths[1])
 
+    def process_data(self, data_path, data_dict):
+        pass
     pass
