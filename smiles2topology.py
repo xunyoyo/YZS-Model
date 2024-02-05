8416d0bbd8dc6b99adae010d2963ba7704d18fcc
xunyoyo <1279416582@qq.com>
2024-02-05 17:24:28 +0800

    测试

--- smiles2topology.py
+++ smiles2topology.py
@@ -1,14 +1,19 @@
 import os.path as op
 import numpy
 import torch
+from torch_geometric.data import InMemoryDataset
 from rdkit import Chem
 from rdkit.Chem import MolFromSmiles
 from tqdm import tqdm
 
 
-class DatasetProcess:
+class GNNDataset(InMemoryDataset):
     def __init__(self):
-        # 这我不会写...
-        pass
+        def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None):
+            super().__init__(root, transform, pre_transform, pre_filter)
+            if train:
+                self.data, self.slices = torch.load(self.processed_paths[0])
+            else:
+                self.data, self.slices = torch.load(self.processed_paths[1])
 
     pass
