624274228179e27355278ce9cccf384be1d2e007
xunyoyo <1279416582@qq.com>
2024-02-05 18:03:22 +0800

    测试

--- smiles2topology.py
+++ smiles2topology.py
@@ -8,7 +8,7 @@ from rdkit.Chem import MolFromSmiles
 from tqdm import tqdm
 
 
-class GNNDataset(InMemoryDataset):
+class MyOwnDataset(InMemoryDataset):
 
     def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None):
         super().__init__(root, transform, pre_transform, pre_filter)
@@ -16,6 +16,17 @@ class GNNDataset(InMemoryDataset):
             self.data, self.slices = torch.load(self.processed_paths[0])
         else:
             self.data, self.slices = torch.load(self.processed_paths[1])
+
+    @property
+    def raw_file_names(self):
+        return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    def download(self):
+        pass
 
     def process_data(self, data_path, data_dict):
         datas = pd.read_csv(data_path)
